12bce8c2cede57be7cd1d3c9de61b5b5c72a0725
Upayan M <upayanm3@gmail.com>
2025-06-20 14:08:09 +0530

    Remove Docker configuration, entrypoint script, and related files

--- entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-
-# Write the VAPID public key to runtime-config.json at container start
-echo "{\n  \"VAPID_PUBLIC_KEY\": \"${NEXT_PUBLIC_VAPID_PUBLIC_KEY:-}\"\n}" > /workspace/app/public/runtime-config.json
-
-# Start PM2 as before
-exec "$@"
